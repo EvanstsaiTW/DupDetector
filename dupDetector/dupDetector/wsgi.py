4861a79ebcc5a76cf48d50eebe37cf83d158e357
EvanstsaiTW <evans.tsai1993@gmail.com>
2020-10-10 17:31:19 -0400

    test website

---
+++ dupDetector/dupDetector/wsgi.py
@@ -0,0 +1,17 @@
+"""
+no need to change
+WSGI config for dupDetector project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dupDetector.settings')
+
+application = get_wsgi_application()
